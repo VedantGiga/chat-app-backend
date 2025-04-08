2da965fe1f13311a9838384ffd58d58dd0055691
VedantGiga <vedantvyas79@gmail.com>
2025-04-08 08:56:42 +0530

    message route fixed

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,14 @@
-import express from 'express'
-import {protectRoute} from '../middlewares/auth.middleware.js'
-import { getUsersForSidebar,getMessages,sendMessage } from '../controllers/message.controller.js';
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/users",protectRoute,getUsersForSidebar)
-router.get("/:id",protectRoute,getMessages)
-router.post("/send/:id",protectRoute,sendMessage)
+router.get("/users", protectRoute, getUsersForSidebar);
 
-export default router
+router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
+
+
+
+export default router;
