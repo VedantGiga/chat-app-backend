3ecf7bb99a5940a1690169440803b8a84dd8e4fb
VedantGiga <vedantvyas79@gmail.com>
2025-06-25 22:23:23 +0530

    vite config fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,10 +26,11 @@ app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../dist")))
+    const frontendPath = path.join(__dirname, "../../frontend/dist")
+    app.use(express.static(frontendPath))
 
     app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname,"../dist", "index.html"))
+        res.sendFile(path.join(frontendPath, "index.html"))
     })
 }
 
