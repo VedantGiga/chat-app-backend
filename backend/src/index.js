2aeee5eb989f1a77f06e7bb0db84c3991db6fca8
VedantGiga <vedantvyas79@gmail.com>
2025-06-25 21:41:10 +0530

    config fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve()
 app.use(express.json({ limit: "10mb" }))
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.NODE_ENV === "production" ? process.env.CLIENT_URL : "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production" ? true : "http://localhost:5173",
     credentials: true,
     methods: ["GET","POST","PUT","DELETE"],
 }))
